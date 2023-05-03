cedffefe0f0351cc1aa9e64640c2880ddb9ea949
Marcos Vinicius <mv.batista2022@gmail.com>
2023-05-03 09:15:09 -0300

    Add projects

---
+++ Intensivo/Exercises-js/main.js
@@ -0,0 +1,72 @@
+// Exercicio 1
+alert('Hello World!')
+
+//Exercicio 2
+let s1 = 1
+let s2 = 2
+const result1 = s1 + s2
+alert(result1)
+
+//Exercicio 3
+let minhaVariavel1 = "a";
+if (typeof minhaVariavel1 === "number") {
+  console.log("A variável é numérica.");
+  alert("numero")
+} else {
+  console.log("A variável não é numérica.");
+  alert(" não numero")
+}
+
+//Exercicio 4
+let minhaVariavel2 = "a";
+if (typeof minhaVariavel2 === "string") {
+  console.log("A variável é string.");
+  alert("string")
+} else {
+  console.log("A variável não é string.");
+  alert(" não string")
+}
+
+//Exercicio 5
+let minhaVariavel3 = "a";
+if (typeof minhaVariavel3 === "boolean") {
+  console.log("A variável é booleana.");
+  alert("booleana")
+} else {
+  console.log("A variável não é booleana.");
+  alert(" não booleana")
+}
+
+//Exercicio 6
+let n1 = 1
+let n2 = 2
+const result2 = n1 - n2
+alert(result2)
+
+//Exercicio 7
+let m1 = 1
+let m2 = 2
+const result3 = m1 * m2
+alert(result3)
+
+//Exercicio 8
+let d1 = 1
+let d2 = 2
+const result4 = d1 / d2
+alert(result4)
+
+//Exercicio 9
+let np = 2
+if(np % 2 == 0){
+    alert("par")
+}else{
+    alert("impar")
+}
+
+//Exercicio 9
+let ni = 2
+if(ni % 2 == 1){
+    alert("impar")
+}else{
+    alert("par")
+}
